e3af3d78cd293a239251002c1f576fba9995b002
geom3trik <geomyles@yahoo.co.uk>
2021-07-09 12:38:47 +0100

    Experiment with lens

--- core/src/widgets/label.rs
+++ core/src/widgets/label.rs
@@ -5,34 +5,40 @@ use std::marker::PhantomData;
 use crate::widgets::*;
 use crate::NodeMap;
 
-pub struct Label<T> {
+pub struct Label {
     text: String,
-    a: PhantomData<*const T>,
+    // a: PhantomData<*const T>,
 
-    converter: Option<Box<dyn Fn(&T) -> String>>
+    // converter: Option<Box<dyn Fn(&T) -> String>>
 }
 
-impl<T> Label<T> {
+impl Label {
     pub fn new(text: &str) -> Self {
         Label {
             text: text.to_string(),
-            a: PhantomData::default(),
+            // a: PhantomData::default(),
 
-            converter: None,
+            // converter: None,
         }
     }
 
-    pub fn with_converter<F>(mut self, converter: F) -> Self 
-    where F: 'static + Fn(&T) -> String
+    pub fn bind<L: Lens, F>(self, something: L, converter: F) -> Wrapper<<L as Lens>::Target, Self> 
+    where F: 'static + Fn(&<L as Lens>::Target) -> <Self as Widget>::Data
     {
-        self.converter = Some(Box::new(converter));
-        self
+        Wrapper::new(self, converter)
     }
+
+    // pub fn with_converter<F>(mut self, converter: F) -> Self 
+    // where F: 'static + Fn(&T) -> String
+    // {
+    //     self.converter = Some(Box::new(converter));
+    //     self
+    // }
 }
 
-impl<T: 'static> Widget for Label<T> {
+impl Widget for Label {
     type Ret = Entity;
-    type Data = T;
+    type Data = String;
     fn on_build(&mut self, state: &mut State, entity: Entity) -> Self::Ret {
         entity
             .set_text(state, &self.text.to_string())
@@ -40,10 +46,63 @@ impl<T: 'static> Widget for Label<T> {
             .set_focusability(state, false)
     }
 
-    fn on_update(&mut self, state: &mut State, entity: Entity, data: &T, nodes: &NodeMap) {
-        if let Some(converter) = &self.converter {
-            self.text = (converter)(data);
+    fn on_update(&mut self, state: &mut State, entity: Entity, data: &Self::Data, nodes: &NodeMap) {
+        //if let Some(converter) = &self.converter {
+            // self.text = (converter)(data);
+            self.text = data.to_owned();
             entity.set_text(state, &self.text);
+        //}
+    }
+}
+
+pub trait Lens {
+
+    type Source;
+    type Target;
+
+    fn view(&self, data: &Self::Source) -> Self::Target;
+}
+
+
+pub struct Wrapper<T, W: Widget> {
+
+    widget: W,
+    converter: Box<dyn Fn(&T) -> <W as Widget>::Data>,
+
+    t: PhantomData<T>,
+    w: PhantomData<W>,
+}
+
+impl<T, W: Widget> Wrapper<T,W> {
+    pub fn new<F>(widget: W, converter: F) -> Self 
+    where F: 'static + Fn(&T) -> <W as Widget>::Data
+    {
+        Self {
+
+            widget,
+            converter: Box::new(converter),
+
+            t: PhantomData::default(),
+            w: PhantomData::default(),
         }
+    }
+}
+
+impl<T: 'static, W: Widget> Widget for Wrapper<T,W> {
+    type Ret = <W as Widget>::Ret;
+    type Data = T;
+
+    fn on_build(&mut self, state: &mut State, entity: Entity) -> Self::Ret {
+        self.widget.on_build(state, entity)
+    }
+
+    fn on_event(&mut self, state: &mut State, entity: Entity, event: &mut Event) {
+        self.widget.on_event(state, entity, event)
+    }
+
+    fn on_update(&mut self, state: &mut State, entity: Entity, data: &Self::Data, nodes: &NodeMap) {
+        // Do something here
+        let value = (self.converter)(data);
+        self.widget.on_update(state, entity, &value, nodes);
     }
 }
--- examples/counter_reactive.rs
+++ examples/counter_reactive.rs
@@ -1,6 +1,6 @@
 use tuix::*;
 use fnv::FnvHashMap;
-use std::any::Any;
+use std::{any::Any, collections::HashSet};
 
 static THEME: &'static str = include_str!("themes/counter_theme.css");
 
@@ -10,9 +10,25 @@ pub struct CounterState {
     value: i32,
 }
 
-impl ToString for CounterState {
-    fn to_string(&self) -> String {
-        self.value.to_string()
+#[derive(Default)]
+pub struct GeneratedLens_CounterState_value;
+
+impl GeneratedLens_CounterState_value {
+    pub const fn new() -> Self {
+        Self {}
+    }
+}
+
+impl CounterState {
+    pub const value: GeneratedLens_CounterState_value = GeneratedLens_CounterState_value::new();
+}
+
+impl Lens for GeneratedLens_CounterState_value {
+    type Source = CounterState;
+    type Target = i32;
+
+    fn view(&self, data: &Self::Source) -> Self::Target {
+        data.value
     }
 }
 
@@ -22,9 +38,15 @@ pub enum CounterMessage {
     Decrement,
 }
 
-impl Node for CounterState {
+impl Widget for CounterState {
+    type Ret = Entity;
+    type Data = ();
 
-    fn on_mutate(&mut self, state: &mut State, entity: Entity, event: &mut Event) {
+    fn on_build(&mut self, state: &mut State, entity: Entity) -> Self::Ret {
+        entity
+    }
+
+    fn on_event(&mut self, state: &mut State, entity: Entity, event: &mut Event) {
         if let Some(counter_event) = event.message.downcast() {
             match counter_event {
                 CounterMessage::Increment => {
@@ -36,9 +58,53 @@ impl Node for CounterState {
                 }
             }            
         }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BindEvent {
+    Bind(Entity),
+    Update,
+}
+
+pub struct Store<D> {
+    data_widget: D,
+    observers: HashSet<Entity>,
+}
+
+impl<D: Widget> Store<D> {
+    pub fn new() -> Self {
 
     }
 }
+
+impl<D: Widget> Widget for Store<D> {
+    type Ret = Entity;
+    type Data = ();
+    fn on_build(&mut self, state: &mut State, entity: Entity) -> Self::Ret {
+        self.data_widget.on_build(state, entity);
+    }
+
+    fn on_event(&mut self, state: &mut State, entity: Entity, event: &mut Event) {
+        if let Some(bind_event) = event.message.downcast() {
+            match bind_event {
+                BindEvent::Bind(target) => {
+                    self.observers.insert(*target);
+                }
+
+                BindEvent::Update => {
+                    for observer in self.observers.iter() {
+                        
+                    }
+                }
+            }
+        }
+
+        self.data_widget.on_event_(state, entity, event);
+    }
+}
+
+
 
 #[derive(Default)]
 struct CounterWidget {
@@ -78,10 +144,14 @@ impl Widget for CounterWidget {
             .build(state, entity, |builder| builder.class("decrement"))
             .bind(state, self.data);
 
-        self.label = Label::<CounterState>::new(&self.value.to_string())
-            .with_converter(|data| data.value.to_string())
-            .build(state, entity, |builder| builder)
-            .bind(state, self.data);
+        // self.label = Label::<CounterState>::new(&self.value.to_string())
+        //     .with_converter(|data| data.value.to_string())
+        //     .build(state, entity, |builder| builder)
+        //     .bind(state, self.data);
+
+        Label::new(&self.value.to_string())
+            .bind(CounterState::value, |value| value.to_string())
+            .build(state, entity, |builder| builder);
 
         entity.set_element(state, "counter").set_layout_type(state, LayoutType::Row)
     }
@@ -101,14 +171,14 @@ fn main() {
         CounterWidget::new(app_data)
             .build(state, window, |builder| builder);
         
-        Label::<CounterState>::new("Zero")
-            .with_converter(|data| english_numbers::convert_all_fmt(data.value as i64))
-            .build(state, window, |builder| 
-                builder
-                    .set_width(Stretch(1.0))
-                    .set_space(Pixels(5.0))
-            )
-            .bind(state, app_data); 
+        // Label::<CounterState>::new("Zero")
+        //     .with_converter(|data| english_numbers::convert_all_fmt(data.value as i64))
+        //     .build(state, window, |builder| 
+        //         builder
+        //             .set_width(Stretch(1.0))
+        //             .set_space(Pixels(5.0))
+        //     )
+        //     .bind(state, app_data); 
     });
 
     app.run();
