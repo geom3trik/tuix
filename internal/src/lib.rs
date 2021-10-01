a3144f3821a8554166007476d1bdcc56743308ca
geom3trik <geomyles@yahoo.co.uk>
2021-10-01 22:51:08 +0100

    attempt to fix baseview dependency

--- internal/src/lib.rs
+++ internal/src/lib.rs
@@ -1,11 +1,8 @@
 
-#[cfg(all(not(feature = "baseview"), not(feature = "glutin"), feature = "winit"))]
-pub use tuix_winit::application::Application;
-
-#[cfg(all(not(feature = "baseview"), not(feature = "winit"), feature = "glutin"))]
+#[cfg(all(not(feature = "baseview"), feature = "glutin"))]
 pub use tuix_glutin::application::Application;
 
-#[cfg(all(not(feature = "glutin"), not(feature = "winit"), feature = "baseview"))]
+#[cfg(all(not(feature = "glutin"), feature = "baseview"))]
 pub use tuix_baseview::Application;
 
 pub use tuix_core::*;
