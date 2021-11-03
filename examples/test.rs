467d9fc39f4d30b46be54fec21f42dbc4e57a2ee
geom3trik <geomyles@yahoo.co.uk>
2021-11-03 22:41:56 +0000

    Update test.rs

--- examples/test.rs
+++ examples/test.rs
@@ -5,20 +5,13 @@ use tuix::widgets::*;
 use tuix::*;
 
 fn main() {
-    let button = Rc::new(RefCell::new(None));
-    let button_ = button.clone();
+
     let app = Application::new(WindowDescription::new(), move |state, window| {
-        *button.borrow_mut() = Some(Button::new().build(state, window, |builder| builder));
+        
     })
     .should_poll()
     .on_idle(move |state| {
-        println!("Tree");
-        // for entity in state.tree.into_iter() {
-        //     println!("Entity: {} Parent: {:?} posx: {} posy: {} width: {} height: {}", entity, entity.parent(&state.tree), state.data.get_posx(entity), state.data.get_posy(entity), state.data.get_width(entity), state.data.get_height(entity));
-        // }
-        // if let Some(button) = button_.borrow_mut().take() {
-        //     state.remove(button);
-        // }
+
     });
     app.run();
 }
